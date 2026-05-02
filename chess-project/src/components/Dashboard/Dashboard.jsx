dbcde5a68880a315ad4cbf5070fa8e2cb9f4e605
Aryan <112301041@smail.iitpkd.ac.in>
2026-05-02 19:59:10 +0530

    made the changes to the replay board , added playername , right side curr pgn move , keybinding eventlistener, back button to route to archive tab of dashboard

--- chess-project/src/components/Dashboard/Dashboard.jsx
+++ chess-project/src/components/Dashboard/Dashboard.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
+import { useLocation } from 'react-router-dom';
 import Sidebar from '../Sidebar/Sidebar';
 import MainContent from '../MainContent/MainContent';
 import './dashboard.css'
 const Dashboard = () => {
-    const [activeTab,setActiveTab] = useState('lobby');
+  const location = useLocation();
+  const initialTab =location.state?.activeTab || 'lobby';
+  const [activeTab,setActiveTab] = useState(initialTab);
   return (
     <div className='dash-container'>
       <Sidebar activeTab={activeTab} setActiveTab={setActiveTab}/>
